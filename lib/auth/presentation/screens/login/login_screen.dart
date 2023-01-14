a648bd0a1c9f0b64d1dfb63db4c910cae18223d6
abdoelfky <aelfky66@gmail.com>
2023-01-14 23:59:26 +0200

    last one

--- lib/auth/presentation/screens/login/login_screen.dart
+++ lib/auth/presentation/screens/login/login_screen.dart
@@ -60,106 +60,105 @@ class LoginScreen extends StatelessWidget {
               }
             },
             builder: (context, state) {
-    return ScreenBackground(
-            child: Center(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(AppPadding.p15),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const LogoWidget(),
-                      const AuthTitleAndSubtitle(
-                        authTitle: AppStrings.login,
-                        authSubtitle: AppStrings.pleaseLogin,
-                      ),
-                      MainTextFormField(
-                        controller: _emailController,
-                        label: AppStrings.email,
-                        hint: AppStrings.emailExample,
-                        hintColor: AppColors.lightGrey,
-                        inputType: TextInputType.emailAddress,
-                        textDirection: TextDirection.ltr,
+              return ScreenBackground(
+                child: Center(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(AppPadding.p15),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const LogoWidget(),
+                          const AuthTitleAndSubtitle(
+                            authTitle: AppStrings.login,
+                            authSubtitle: AppStrings.pleaseLogin,
+                          ),
+                          MainTextFormField(
+                            controller: _emailController,
+                            label: AppStrings.email,
+                            hint: AppStrings.emailExample,
+                            hintColor: AppColors.lightGrey,
+                            inputType: TextInputType.emailAddress,
+                            textDirection: TextDirection.ltr,
 
-                        isObsecured:false,
-                        validator: (value) => validateEmail(value!),
-                      ),
-                      SizedBox(height: mediaQueryHeight(context) / AppSize.s30),
-                      MainTextFormField(
-                        isObsecured:LoginCubit.get(context).isObsecured ,
-                        suffixIcon: IconButton(
-                            color: Colors.white,
-                            icon: LoginCubit.get(context).isObsecured?Icon(Icons.visibility,color: AppColors.primary,):
-                            Icon(Icons.visibility_off,color: AppColors.primary,),
-                            onPressed: (){
-                              LoginCubit.get(context).changeVisibility();
-                            }),
-                        controller: _passwordController,
-                        label: AppStrings.password,
-                        hint: AppStrings.passwordExample,
-                        hintColor: AppColors.lightGrey,
-                        inputType: TextInputType.visiblePassword,
-                        textDirection: TextDirection.ltr,
-                        validator: (value) {
-                          if (value!.length < AppSize.s8) {
-                            return AppStrings.enterValidPassword;
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {
-                            navigateTo(
-                              context: context,
-                              screenRoute: Routes.forgetPasswordScreen,
-                            );
-                          },
-                          child: Text(
-                            AppStrings.forgotPassword,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelMedium!
-                                .copyWith(
+                            isObsecured:false,
+                            validator: (value) => validateEmail(value!),
+                          ),
+                          SizedBox(height: mediaQueryHeight(context) / AppSize.s30),
+                          MainTextFormField(
+                            isObsecured:LoginCubit.get(context).isObsecured ,
+                            suffixIcon: IconButton(
+                                color: Colors.white,
+                                icon: LoginCubit.get(context).isObsecured?Icon(Icons.visibility,color: AppColors.primary,):
+                                Icon(Icons.visibility_off,color: AppColors.primary,),
+                                onPressed: (){
+                                  LoginCubit.get(context).changeVisibility();
+                                }),
+                            controller: _passwordController,
+                            label: AppStrings.password,
+                            hint: AppStrings.passwordExample,
+                            hintColor: AppColors.lightGrey,
+                            inputType: TextInputType.visiblePassword,
+                            textDirection: TextDirection.ltr,
+                            validator: (value) {
+                              if (value!.length < AppSize.s8) {
+                                return AppStrings.enterValidPassword;
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {
+                                navigateTo(
+                                  context: context,
+                                  screenRoute: Routes.forgetPasswordScreen,
+                                );
+                              },
+                              child: Text(
+                                AppStrings.forgotPassword,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelMedium!
+                                    .copyWith(
                                   color: AppColors.yellowBold,
                                 ),
+                              ),
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            height: mediaQueryHeight(context) / AppSize.s80,
+                          ),
+                          ConditionalBuilder(condition:(state is LoginLoadingState),
+                              builder: (context)=>const CircularProgressIndicator(),
+                              fallback: (context)=>MainButton(
+                                title: AppStrings.login,
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    LoginCubit.get(context).userLogin(
+                                        email: _emailController.text.trim(),
+                                        password: _passwordController.text);
+                                  }
+                                },
+                              )
+                          ),
+
+                        ],
                       ),
-                      SizedBox(
-                        height: mediaQueryHeight(context) / AppSize.s80,
-                      ),
-                  ConditionalBuilder(condition:(state is LoginLoadingState),
-                    builder: (context)=>const CircularProgressIndicator(),
-                    fallback: (context)=>MainButton(
-                      title: AppStrings.login,
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          LoginCubit.get(context).userLogin(
-                              email: _emailController.text.trim(),
-                              password: _passwordController.text);
-                        }
-                      },
-                    )
-                ),
-                      
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-          );
-  },
-),
+              );
+            },
+          ),
         ),
       ),
     );
   }
 }
-
 
 
